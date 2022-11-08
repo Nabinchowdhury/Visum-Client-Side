fd37bafc258008dd625568600cfc6a54a27b237a
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-11-09 03:22:25 +0600

    conditional review section initialized

--- src/Components/Pages/Services/Services.js
+++ src/Components/Pages/Services/Services.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider';
 import ServiceCard from '../../Shared/ServiceCard';
 
 const Services = () => {
+    const { user } = useContext(AuthContext)
     const [services, setServices] = useState([])
     useEffect(() => {
         fetch("http://localhost:5000/services")
@@ -19,6 +21,18 @@ const Services = () => {
                     {
                         services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
                     }
+                </div>
+
+            </div>
+
+
+            <div className='my-32'>
+                <h2 className='text-4xl my-10'>Reviews</h2>
+                {
+                    user ? <textarea name="details" className="textarea textarea-bordered h-24 w-full" placeholder="Add a Review" required></textarea> : "Please login to add review"
+                }
+                <div className='grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:mx-32'>
+
                 </div>
 
             </div>
